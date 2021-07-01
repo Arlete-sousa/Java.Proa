b411ff1a87fe7587e1c79d21d02efb459892d939
Arlete_Sousa <81167437+Arlete-sousa@users.noreply.github.com>
2021-07-01 16:05:16 -0300

    Add files via upload

---
+++ onibus.java
@@ -0,0 +1,36 @@
+class Onibus {
+    int quantidade_acentos;
+    int capacidade_pessoas;
+    int quantidade_pessoas;
+    int quantidade_paradas;
+    String local;
+       
+    public Onibus(int quantidade_acentos, int capacidade_pessoas,int quantidade_pessoas, int quantidade_paradas, String local) {
+
+        this.quantidade_acentos = quantidade_acentos;
+        this.capacidade_pessoas = capacidade_pessoas;
+        this.quantidade_pessoas = quantidade_pessoas;
+        this.quantidade_paradas = quantidade_paradas;
+        this.local = local;
+    }
+
+    public int returnQuantidade_acentos(){
+        return this.quantidade_acentos;
+    }
+    
+    public int returnCapacidade_pessoas(){
+        return this.capacidade_pessoas;
+    }
+
+    public int returnQuantidade_pessoas(){
+        return this.quantidade_pessoas;
+    }
+    
+    public int returnQuantidade_paradas(){
+        return this.quantidade_paradas;
+    }
+
+    public String returnLocal(){
+        return this.local;
+    }
+}
---
+++ programaOnibus.java
@@ -0,0 +1,40 @@
+public class programaOnibus{
+    public static void main(String[] args) {   
+        Onibus Onibus1 = new Onibus(28, 30, 22, 8);
+        Onibus Onibus2 = new Onibus(28, 30, 24, 8);
+        Onibus Onibus3 = new Onibus(28, 30, 26, 8);
+
+        Onibus1.liga();
+        System.out.println("Passageiros, sejam muito bem vindos!");
+        System.out.println("Podem entrar, e se assentar em suas assentos");
+        System.out.println("Espero que gostem de nossa viagem :) ");
+        Onibus1.entrarPassageiros();
+
+        Onibus2.liga();
+        System.out.println("Passageiros, sejam bem vindos!");
+        System.out.println("Podem entrar, e se assentar em suas assentos");
+        Onibus2.entrarPassageiros();
+
+        Onibus3.liga();
+        System.out.println("Passageiros, sejam muito bem vindos!");
+        System.out.println("Vocês são muito especiais para nós");
+        System.out.println("Podem entrar, e se assentar em suas assentos");
+        System.out.println("Espero que gostem de nossa viagem :) ");
+        Onibus3.entrarPassageiros();
+
+        Onibus1.PararPrimeiraParada();
+        Onibus1.descerPassageiros();
+        int quantidade_acentos = 0;
+        System.out.println("aqui desceu 14 pessoas, agora no total temos " + (quantidade_acentos - 14));
+
+        Onibus2.PararPrimeiraParada();
+        Onibus2.descerPassageiros();
+        System.out.println("aqui desceu 8 pessoas, agora no total temos " + (quantidade_acentos - 8));
+
+        Onibus2.PararPrimeiraParada();
+        Onibus1.descerPassageiros();
+        System.out.println("aqui desceu 11 pessoas, agora no total temos " + (quantidade_acentos - 11));
+
+
+}
+}
